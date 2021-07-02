64b0f3a61625028847842c855cf1b6d88bcff6d2
WilliamKXiong <mana_grey@hotmail.com>
2021-07-01 23:00:27 -0500

    removed remote mysql

--- src/main/java/com/jwt/controllers/HomePageController.java
+++ src/main/java/com/jwt/controllers/HomePageController.java
@@ -1,13 +1,12 @@
 package com.jwt.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomePageController {
-	
-	//677509
+
+
 	@RequestMapping("/")
 	public String index() {
 
